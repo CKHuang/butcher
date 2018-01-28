7013e1afaa15d7345eb99cedcd0e4e518385c102
Huang.CK <549596429@qq.com>
2018-01-28 20:53:27 +0800

    update

--- src/lib/Project.ts
+++ src/lib/Project.ts
@@ -5,12 +5,12 @@ import GitClient from './cvs/GitClient';
 import SvnClient from './cvs/SvnClient';
 import debug from './debug';
 import config from '../config/app'
-import code from '../config/code'
+import Code from '../config/code'
 import { Repository } from './cvs/ClientBase';
 import ExtError from './ExtError';
 
 interface IProject {
-    initialize(remotePath:string):Promise<boolean>
+    // initialize(remotePath:string):Promise<boolean>
 }
 
 export enum ProjectTypes {
@@ -21,41 +21,28 @@ export enum ProjectTypes {
 export class Project extends EventEmitter implements IProject {
     private cvs:GitClient|SvnClient|null = null;
     private type:ProjectTypes;
-    constructor(type:ProjectTypes) {
+    constructor(type:ProjectTypes,remotePath:string) {
         super();
         this.type = type;
+        debug.info('project init CVS');
+        debug.info('remotePath : ' + remotePath);
+        debug.info('type : ' + this.type);
+        switch( this.type ) {
+            case 'Svn' : this.cvs = new SvnClient({
+                name : 'Default',
+                password : config.svnAccount.password,
+                username : config.svnAccount.username,
+                remotePath : remotePath,
+                localPath : "unknow"
+            });break;
+            case 'Git' : this.cvs = new GitClient({
+                name : 'Default',
+                password : config.gitAccount.password,
+                username : config.gitAccount.username,
+                remotePath : remotePath,
+                localPath : "unknow"
+            });
+        }
         return this;
     }
-    initialize(remotePath:string):Promise<boolean> {
-        const me = this;
-        return new Promise((resolve,reject) => {
-            debug.info('project initialize');
-            debug.info('remotePath : '+remotePath);
-            switch( me.type ) {
-                case 'Svn' : me.cvs = new SvnClient({
-                    name : 'Default',
-                    password : config.svnAccount.password,
-                    username : config.svnAccount.username,
-                    remotePath : remotePath,
-                    localPath  : "unknow"
-                });
-                case 'Git' : me.cvs = new GitClient({
-                    name : 'Default',
-                    password : config.gitAccount.password,
-                    username : config.gitAccount.username,
-                    remotePath : remotePath,
-                    localPath  : 'unknow'
-                });
-            }
-            if ( !me.cvs ) { 
-                let error = new ExtError('project init cvs error');
-                    error.code = code.project_init_cvs_error;
-                    error.args = remotePath;
-                reject();
-                return ;
-            }
-            resolve();
-        });
-    }
-
 }
