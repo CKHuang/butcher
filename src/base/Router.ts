e3a4aaf3fe40d0b7bb9b42f8a4a572bd18de009c
kavinkhuang <kavinkhuang@tencent.com>
2018-02-09 18:02:20 +0800

    update

--- src/base/Router.ts
+++ src/base/Router.ts
@@ -1,9 +1,25 @@
 "use strict";
 
 import * as KoaRouter from 'koa-router'
+import { Logger, LoggerLevel } from '../utils/Logger'
 
-export abstract class Router extends KoaRouter {
+export enum RouterMethod {
+    GET  = "get",
+    POST = 'post',
+    ALL  = 'all',
+    HEAD = 'head'
+}
+
+export class Router extends KoaRouter {
     constructor(...args:any[]) {
        super(...args);
+    }
+    private willFire(ctx:KoaRouter.IRouterContext) {
+        Logger.info(LoggerLevel.APP,'willFire router action');
+        Logger.info(LoggerLevel.APP,ctx);
+    }
+    add ( method:RouterMethod, path:string, action:KoaRouter.IMiddleware ) {
+        let fn = this[method];
+        fn && fn(path,this.willFire,action);
     }
 }
--- src/module/router/ApiRouter.ts
+++ src/module/router/ApiRouter.ts
@@ -1,20 +1,20 @@
 "use strict";
 
-import { Router } from '../../base/Router';
+import { Router, RouterMethod } from '../../base/Router';
 import * as KoaRouter from 'koa-router';
 import * as Koa from 'koa';
 import ApiController from '../controller/ApiController'
 
 export default class ApiRouter extends Router {
+    /**
+     * Api控制器
+     * @ApiController
+     */
     private apiCtrl:ApiController;
     constructor() {
         super();
         this.apiCtrl = new ApiController();
         this.prefix('/api');
-        this.registerRoutes();
-    }
-
-    registerRoutes() {
-        this.get('/name',this.apiCtrl.name);
+        this.add(RouterMethod.GET,'/name',this.apiCtrl.name)
     }
 }
